ecc4bee7a8edb811c789d9d285b7e6569385270f
RodelDecio <104150864+RodelDecio@users.noreply.github.com>
2023-10-20 20:27:36 -0700

    html, css and js

---
+++ Online Store/storeitem.js
@@ -0,0 +1,494 @@
+const storeItem= [
+    {
+    id: 1,
+    name: 'Universal Remote',
+    category : 'appliances',
+    rating : 4.3,
+    price: 1.76,
+    img: 'images/Appliances/ap1.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'LED Mini projector',
+    category : 'appliances',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Appliances/ap.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Clip Fan',
+    category : 'appliances',
+    rating : 4.3,
+    price: 3,
+    img: 'images/Appliances/ap2.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Electric Stove',
+    category : 'appliances',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Appliances/ap3.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mini Sewing Machine',
+    category : 'appliances',
+    rating : 4.3,
+    price: 11,
+    img: 'images/Appliances/ap4.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Boiling Electric Kettle',
+    category : 'appliances',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Appliances/ap5.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Electric Hand Mixer',
+    category : 'appliances',
+    rating : 4.3,
+    price: 13,
+    img: 'images/Appliances/ap6.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Bluetooth Speaker',
+    category : 'appliances',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Appliances/ap7.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Audio Amplifier',
+    category : 'appliances',
+    rating : 4.3,
+    price: 14,
+    img: 'images/Appliances/ap8.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Vivo Y85',
+    category : 'electronic',
+    rating : 4.3,
+    price: 140,
+    img: 'images/Electronic Devices & Accessories/cp.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Oppo A31',
+    category : 'electronic',
+    rating : 4.3,
+    price: 140,
+    img: 'images/Electronic Devices & Accessories/cp1.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Infinix Note30',
+    category : 'electronic',
+    rating : 4.3,
+    price: 140,
+    img: 'images/Electronic Devices & Accessories/cp2.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Nokia 2610',
+    category : 'electronic',
+    rating : 4.3,
+    price: 13,
+    img: 'images/Electronic Devices & Accessories/cp3.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Note12',
+    category : 'electronic',
+    rating : 4.3,
+    price: 140,
+    img: 'images/Electronic Devices & Accessories/cp4.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Car Phone Holder',
+    category : 'electronic',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Electronic Devices & Accessories/cp5.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Headset',
+    category : 'electronic',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Electronic Devices & Accessories/cp6.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Wireless Earphone',
+    category : 'electronic',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Electronic Devices & Accessories/cp7.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Universal Earphone',
+    category : 'electronic',
+    rating : 4.3,
+    price: 6,
+    img: 'images/Electronic Devices & Accessories/cp8.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Longsleeves',
+    category : 'fashion',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Fashion & Accessories/fashion.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Maxi Skirt',
+    category : 'fashion',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Fashion & Accessories/fashion1.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Casual shoes',
+    category : 'fashion',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Fashion & Accessories/fashion2.jpg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Doll shoes',
+    category : 'fashion',
+    rating : 4.3,
+    price: 6,
+    img: 'images/Fashion & Accessories/fashion3.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Cross Bag',
+    category : 'fashion',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Fashion & Accessories/fashion4.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Shoulder Bag',
+    category : 'fashion',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Fashion & Accessories/fashion5.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Couple Watches',
+    category : 'fashion',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Fashion & Accessories/fashion6.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Basketball Shoes',
+    category : 'fashion',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Fashion & Accessories/fashion7.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'T-shirts',
+    category : 'fashion',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Fashion & Accessories/fashion8.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Bundle Grocery',
+    category : 'groceries',
+    rating : 4.3,
+    price: 6,
+    img: 'images/Groceries/groceries.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Native Garlic',
+    category : 'groceries',
+    rating : 4.3,
+    price: 2,
+    img: 'images/Groceries/groceries1.jpg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Century',
+    category : 'groceries',
+    rating : 4.3,
+    price: 3,
+    img: 'images/Groceries/groceries2.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: '555 Sardines',
+    category : 'groceries',
+    rating : 4.3,
+    price: 3,
+    img: 'images/Groceries/groceries3.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: '5pcs Spam',
+    category : 'groceries',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Groceries/groceries4.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Purefoods Nuggets',
+    category : 'groceries',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Groceries/groceries5.jpg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Pik-Nik',
+    category : 'groceries',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Groceries/groceries6.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Sayote 1kg',
+    category : 'groceries',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Groceries/groceries7.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Lemon 100G',
+    category : 'groceries',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Groceries/groceries8.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Makeup Powder',
+    category : 'health',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Health & Beauty/health.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Concealer',
+    category : 'health',
+    rating : 4.3,
+    price: 6,
+    img: 'images/Health & Beauty/health1.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Korean Soap',
+    category : 'health',
+    rating : 4.3,
+    price: 6,
+    img: 'images/Health & Beauty/health2.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Aloe vera Conditioner',
+    category : 'health',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Health & Beauty/health3.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Avon lotion',
+    category : 'health',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Health & Beauty/health4.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Luxx White',
+    category : 'health',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Health & Beauty/health5.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: '5pcs Salonpas',
+    category : 'health',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Health & Beauty/health6.jpg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Gillette',
+    category : 'health',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Health & Beauty/health7.jpg',
+    quantity: 1
+},
+{
+    id: 45,
+    name: '3 in 1 Sets',
+    category : 'health',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Health & Beauty/health8.jpg',
+    quantity: 1
+},
+{
+    id: 46,
+    name: 'Mountain Bike',
+    category : 'sports',
+    rating : 4.3,
+    price: 125,
+    img: 'images/Sports & Lifestyle/sl.jpg',
+    quantity: 1
+},
+{
+    id: 47,
+    name: 'Camping Tent',
+    category : 'sports',
+    rating : 4.3,
+    price: 20,
+    img: 'images/Sports & Lifestyle/sl1.jpg',
+    quantity: 1
+},
+{
+    id: 48,
+    name: 'Korean Bike',
+    category : 'sports',
+    rating : 4.3,
+    price: 70,
+    img: 'images/Sports & Lifestyle/sl2.jpg',
+    quantity: 1
+},
+{
+    id: 49,
+    name: 'Pilipinas Jersey',
+    category : 'sports',
+    rating : 4.3,
+    price: 20,
+    img: 'images/Sports & Lifestyle/sl3.jpg',
+    quantity: 1
+},
+{
+    id: 50,
+    name: 'Jumping Roop',
+    category : 'sports',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Sports & Lifestyle/sl4.jpg',
+    quantity: 1
+},
+{
+    id: 51,
+    name: 'Yoga Ball',
+    category : 'sports',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Sports & Lifestyle/sl5.jpg',
+    quantity: 1
+},
+{
+    id: 52,
+    name: 'Classic Guitar',
+    category : 'sports',
+    rating : 4.3,
+    price: 70,
+    img: 'images/Sports & Lifestyle/sl6.jpg',
+    quantity: 1
+},
+{
+    id: 53,
+    name: 'Basketball ball',
+    category : 'sports',
+    rating : 4.3,
+    price: 20,
+    img: 'images/Sports & Lifestyle/sl7.jpg',
+    quantity: 1
+},
+{
+    id: 54,
+    name: 'Nonstick Gloves',
+    category : 'sports',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Sports & Lifestyle/sl8.jpg',
+    quantity: 1
+},
+
+]
+
+export {storeItem};
